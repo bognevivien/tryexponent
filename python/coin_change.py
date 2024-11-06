ea356f4080e6905b8635c8b8ee3b8d820d36154e
vivien <bognetienoue@gmail.com>
2024-11-06 12:38:36 +0100

    coin_change

--- python/coin_change.py
+++ python/coin_change.py
@@ -2,15 +2,14 @@ from typing import List
 
 
 def coin_change(amount: int, coins: List[int])-> int:
-    dp =[amount+1 for _ in range(amount+1)]
-    print(dp, len(dp))
+    dp =[float('inf') for _ in range(amount+1)]
     dp[0]  = 0
     for a in range(1, amount+1):
         for c in coins:
             if a-c >= 0:
                 dp[a] = min(dp[a], 1+ dp[a-c])
     
-    return dp[amount]
+    return dp[amount] if dp[amount] != float('inf') else -1
 
 
 
